80dfdaaa38847c832bec7f8043ee5e3d1cf0a798
ssu20192650 <unbroken2650@gmail.com>
2023-04-16 15:25:58 +0900

    HW3 Done

--- HW3/multiHW.c
+++ HW3/multiHW.c
@@ -6,11 +6,12 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
+#include <signal.h>
 
 void errProc();
 void errPrint();
-void child_handler(int sig);
-int main(int argc, char** argv)
+void sigHandler(int sig);
+int main(int argc, char **argv)
 {
 	int srvSd, clntSd;
 	struct sockaddr_in srvAddr, clntAddr;
@@ -18,58 +19,66 @@ int main(int argc, char** argv)
 	char rBuff[BUFSIZ];
 	pid_t pid;
 
-	signal(SIGCHLD, (void *)child_handler);
+	// 시그널(SIGCHLD)가 발생하면 실행할 함수(sigHandler)를 지정
+	signal(SIGCHLD, (void *)sigHandler);
 
-	if(argc != 2) {
+	if (argc != 2)
+	{
 		printf("Usage: %s [port] \n", argv[0]);
 		exit(1);
 	}
 	printf("Server start...\n");
 
 	srvSd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if(srvSd == -1 ) errProc();
+	if (srvSd == -1)
+		errProc();
 
 	memset(&srvAddr, 0, sizeof(srvAddr));
 	srvAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	srvAddr.sin_family = AF_INET;
 	srvAddr.sin_port = htons(atoi(argv[1]));
 
-	if(bind(srvSd, (struct sockaddr *) &srvAddr, sizeof(srvAddr)) == -1)
+	if (bind(srvSd, (struct sockaddr *)&srvAddr, sizeof(srvAddr)) == -1)
 		errProc();
-	if(listen(srvSd, 5) < 0)
+	if (listen(srvSd, 5) < 0)
 		errProc();
 	clntAddrLen = sizeof(clntAddr);
-	while(1)
+	while (1)
 	{
-		clntSd = accept(srvSd, (struct sockaddr *)
-					 &clntAddr, &clntAddrLen);
-		if(clntSd == -1) {
+		clntSd = accept(srvSd, (struct sockaddr *)&clntAddr, &clntAddrLen);
+		if (clntSd == -1)
+		{
 			errPrint();
-			continue;	
+			continue;
 		}
 		printf("client %s:%d is connected...\n",
-			inet_ntoa(clntAddr.sin_addr),
-			ntohs(clntAddr.sin_port));	
+					 inet_ntoa(clntAddr.sin_addr),
+					 ntohs(clntAddr.sin_port));
 		pid = fork();
-		if(pid == 0) { /* child process */
+		if (pid == 0)
+		{ /* child process */
 			close(srvSd);
-			while(1) {
-				readLen = read(clntSd, rBuff, sizeof(rBuff)-1);
-				if(readLen == 0) break;
+			while (1)
+			{
+				readLen = read(clntSd, rBuff, sizeof(rBuff) - 1);
+				if (readLen == 0)
+					break;
 				rBuff[readLen] = '\0';
 				printf("Client(%d): %s\n",
-					ntohs(clntAddr.sin_port),rBuff);
+							 ntohs(clntAddr.sin_port), rBuff);
 				write(clntSd, rBuff, strlen(rBuff));
 			}
 			printf("Client(%d): is disconnected\n",
-				ntohs(clntAddr.sin_port));
+						 ntohs(clntAddr.sin_port));
 			close(clntSd);
 			return 0;
-		}	
-		else if(pid == -1) errProc("fork");
-		else { /*Parent Process*/
+		}
+		else if (pid == -1)
+			errProc("fork");
+		else
+		{ /*Parent Process*/
 			close(clntSd);
-		}	
+		}
 	}
 	close(srvSd);
 	return 0;
@@ -77,20 +86,21 @@ int main(int argc, char** argv)
 
 void errProc(const char *str)
 {
-    fprintf(stderr,"%s: %s \n",str, strerror(errno));
-    exit(1);
+	fprintf(stderr, "%s: %s \n", str, strerror(errno));
+	exit(1);
 }
 
 void errPrint(const char *str)
 {
-	fprintf(stderr,"%s: %s \n",str, strerror(errno));
+	fprintf(stderr, "%s: %s \n", str, strerror(errno));
 }
 
-void sigint_handler(int sig)
+void sigHandler(int sig)
 {
 	pid_t pid;
 	int stat;
 
+	// wait()를 통해 Child Process가 종료되었는지 확인
 	pid = wait(&stat);
 	printf("Child %d Terminated.\n", pid);
 	return;
