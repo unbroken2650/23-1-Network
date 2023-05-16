6f2ecc6fffba4fcdd5467d76c286aac7e621ffc3
ssu20192650 <unbroken2650@gmail.com>
2023-05-16 20:07:08 +0900

    HW4 start

--- HW4/tcp_server.c
+++ HW4/tcp_server.c
@@ -1,22 +1,20 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netdb.h>
-#include <unistd.h>
+#include <ctype.h>
 #include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define ISVALIDSOCKET(s) ((s) >= 0)
 #define CLOSESOCKET(s) close(s)
 #define SOCKET int
 #define GETSOCKETERRNO() (errno)
 
-
 int main() {
-
     printf("Configuring local address...\n");
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
@@ -27,25 +25,20 @@ int main() {
     struct addrinfo *bind_address;
     getaddrinfo(0, "8080", &hints, &bind_address);
 
-
     printf("Creating socket...\n");
     SOCKET socket_listen;
-    socket_listen = socket(bind_address->ai_family,
-            bind_address->ai_socktype, bind_address->ai_protocol);
+    socket_listen = socket(bind_address->ai_family, bind_address->ai_socktype, bind_address->ai_protocol);
     if (!ISVALIDSOCKET(socket_listen)) {
         fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
     }
 
-
     printf("Binding socket to local address...\n");
-    if (bind(socket_listen,
-                bind_address->ai_addr, bind_address->ai_addrlen)) {
+    if (bind(socket_listen, bind_address->ai_addr, bind_address->ai_addrlen)) {
         fprintf(stderr, "bind() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
     }
     freeaddrinfo(bind_address);
-
 
     printf("Listening...\n");
     if (listen(socket_listen, 10) < 0) {
@@ -60,41 +53,34 @@ int main() {
 
     printf("Waiting for connections...\n");
 
-
-    while(1) {
+    while (1) {
         fd_set reads;
         reads = master;
-        if (select(max_socket+1, &reads, 0, 0, 0) < 0) {
+        if (select(max_socket + 1, &reads, 0, 0, 0) < 0) {
             fprintf(stderr, "select() failed. (%d)\n", GETSOCKETERRNO());
             return 1;
         }
 
         SOCKET i;
-        for(i = 1; i <= max_socket; ++i) {
+        for (i = 1; i <= max_socket; ++i) {
             if (FD_ISSET(i, &reads)) {
-
                 if (i == socket_listen) {
                     struct sockaddr_storage client_address;
                     socklen_t client_len = sizeof(client_address);
-                    SOCKET socket_client = accept(socket_listen,
-                            (struct sockaddr*) &client_address,
-                            &client_len);
+                    SOCKET socket_client = accept(socket_listen, (struct sockaddr *)&client_address, &client_len);
                     if (!ISVALIDSOCKET(socket_client)) {
-                        fprintf(stderr, "accept() failed. (%d)\n",
-                                GETSOCKETERRNO());
+                        fprintf(stderr, "accept() failed. (%d)\n", GETSOCKETERRNO());
                         return 1;
                     }
 
                     FD_SET(socket_client, &master);
-                    if (socket_client > max_socket)
-                        max_socket = socket_client;
+                    if (socket_client > max_socket) max_socket = socket_client;
 
                     char address_buffer[100];
-                    getnameinfo((struct sockaddr*)&client_address,
-                            client_len,
-                            address_buffer, sizeof(address_buffer), 0, 0,
-                            NI_NUMERICHOST);
-                    printf("New connection from %s\n", address_buffer);
+                    char port_buffer[10];
+                    getnameinfo((struct sockaddr *)&client_address, client_len, address_buffer, sizeof(address_buffer), port_buffer, sizeof(port_buffer), NI_NUMERICHOST | NI_NUMERICSERV);
+
+                    printf("New connection from port %s\n", port_buffer);
 
                 } else {
                     char read[1024];
@@ -105,17 +91,19 @@ int main() {
                         continue;
                     }
 
-                    int j;
-                    for (j = 0; j < bytes_received; ++j)
-                        read[j] = toupper(read[j]);
-                    send(i, read, bytes_received, 0);
+                    SOCKET j;
+                    for (j = 1; j <= max_socket; ++j) {
+                        if (FD_ISSET(j, &master)) {
+                            if (j == socket_listen || j == i)
+                                continue;
+                            else
+                                send(j, read, bytes_received, 0);
+                        }
+                    }
                 }
-
-            } //if FD_ISSET
-        } //for i to max_socket
-    } //while(1)
-
-
+            }  // if FD_ISSET
+        }      // for i to max_socket
+    }          // while(1)
 
     printf("Closing listening socket...\n");
     CLOSESOCKET(socket_listen);
