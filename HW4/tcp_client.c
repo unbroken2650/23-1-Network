3e7658f72cc8b58cd5fbb091e6b5b339489273fb
ssu20192650 <unbroken2650@gmail.com>
2023-05-16 21:42:39 +0900

    HW4 sending port number each other in server

--- HW4/tcp_client.c
+++ HW4/tcp_client.c
@@ -1,13 +1,13 @@
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netdb.h>
-#include <unistd.h>
 #include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define ISVALIDSOCKET(s) ((s) >= 0)
 #define CLOSESOCKET(s) close(s)
@@ -15,71 +15,75 @@
 #define GETSOCKETERRNO() (errno)
 
 int main(int argc, char *argv[]) {
-
-
+    // 명령줄 인수 확인
     if (argc < 3) {
         fprintf(stderr, "usage: tcp_client hostname port\n");
         return 1;
     }
 
     printf("Configuring remote address...\n");
+
+    // 주소 정보 힌트 설정
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
     hints.ai_socktype = SOCK_STREAM;
+
+    // 지정된 호스트명과 포트에 대한 주소 정보 가져오기
     struct addrinfo *peer_address;
     if (getaddrinfo(argv[1], argv[2], &hints, &peer_address)) {
         fprintf(stderr, "getaddrinfo() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
     }
 
-
     printf("Remote address is: ");
     char address_buffer[100];
     char service_buffer[100];
-    getnameinfo(peer_address->ai_addr, peer_address->ai_addrlen,
-            address_buffer, sizeof(address_buffer),
-            service_buffer, sizeof(service_buffer),
-            NI_NUMERICHOST);
+
+    // 주소 정보를 인간이 읽을 수 있는 형식으로 변환
+    getnameinfo(peer_address->ai_addr, peer_address->ai_addrlen, address_buffer, sizeof(address_buffer), service_buffer, sizeof(service_buffer), NI_NUMERICHOST);
+
     printf("%s %s\n", address_buffer, service_buffer);
 
-
-    printf("Creating socket...\n");
+    printf("Creating Socket...\n");
     SOCKET socket_peer;
-    socket_peer = socket(peer_address->ai_family,
-            peer_address->ai_socktype, peer_address->ai_protocol);
+
+    // 주소 정보를 사용하여 소켓 생성
+    socket_peer = socket(peer_address->ai_family, peer_address->ai_socktype, peer_address->ai_protocol);
     if (!ISVALIDSOCKET(socket_peer)) {
         fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
     }
 
-
     printf("Connecting...\n");
-    if (connect(socket_peer,
-                peer_address->ai_addr, peer_address->ai_addrlen)) {
+
+    // 소켓을 원격 주소에 연결
+    if (connect(socket_peer, peer_address->ai_addr, peer_address->ai_addrlen)) {
         fprintf(stderr, "connect() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
     }
+
     freeaddrinfo(peer_address);
 
     printf("Connected.\n");
     printf("To send data, enter text followed by enter.\n");
 
-    while(1) {
-
+    while (1) {
+        // select()를 위한 파일 디스크립터 설정
         fd_set reads;
         FD_ZERO(&reads);
-        FD_SET(socket_peer, &reads);
-        FD_SET(0, &reads);
+        FD_SET(socket_peer, &reads);  // socket_peer을 집합에 추가
+        FD_SET(0, &reads);            // 표준 입력(0)을 집합에 추가
 
+        // select()의 타임아웃 설정
         struct timeval timeout;
         timeout.tv_sec = 0;
         timeout.tv_usec = 100000;
 
-        if (select(socket_peer+1, &reads, 0, 0, &timeout) < 0) {
+        // 소켓이나 표준 입력에서 활동을 감지
+        if (select(socket_peer + 1, &reads, 0, 0, &timeout) < 0) {
             fprintf(stderr, "select() failed. (%d)\n", GETSOCKETERRNO());
             return 1;
         }
-
         if (FD_ISSET(socket_peer, &reads)) {
             char read[4096];
             int bytes_received = recv(socket_peer, read, 4096, 0);
@@ -87,18 +91,17 @@ int main(int argc, char *argv[]) {
                 printf("Connection closed by peer.\n");
                 break;
             }
-            printf("Received (%d bytes): %.*s",
-                    bytes_received, bytes_received, read);
+            printf("Received Data (%d bytes): %.*s", bytes_received, bytes_received, read);
         }
 
-        if(FD_ISSET(0, &reads)) {
+        if (FD_ISSET(0, &reads)) {
             char read[4096];
             if (!fgets(read, 4096, stdin)) break;
             printf("Sending: %s", read);
             int bytes_sent = send(socket_peer, read, strlen(read), 0);
             printf("Sent %d bytes.\n", bytes_sent);
         }
-    } //end while(1)
+    }  // end while(1)
 
     printf("Closing socket...\n");
     CLOSESOCKET(socket_peer);
@@ -106,4 +109,3 @@ int main(int argc, char *argv[]) {
     printf("Finished.\n");
     return 0;
 }
-
